e9b19b5451b4d32d33c00369ace9b803f7f65e2b
oscar-wolfbrandt <142879263+oscar-wolfbrandt@users.noreply.github.com>
2023-12-05 11:03:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction() { 
+    document.getElementById("jtest").innerHTML = "test test";
+  }
+
+  function Functionone() { 
+    document.getElementById("jtestow").style.fontSize = "25px";
+    document.getElementById("jtestow").style.color = "red";
+    document.getElementById("jtestow").style.backgroundColor = "yellow";
+  }
+function revert(){
+    document.getElementById("jtestow").style.fontSize = "1rem";
+    document.getElementById("jtestow").style.color = "black";
+    document.getElementById("jtestow").style.backgroundColor = "white";
+
+}
